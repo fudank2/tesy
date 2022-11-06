845c8f1654690b96d3c44bad34762e6b58b72081
Fudan Kumar <Fudank2498@gmail.com>
2022-11-06 11:55:00 +0530

    this is added

---
+++ cf3.cpp
@@ -0,0 +1,88 @@
+
+#include<bits/stdc++.h>
+using namespace std; 
+#define ll              long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define pll_ll          pair<long long,long long>
+#define vi              vector<int>
+#define vll            vector<long long>
+#define si             set<int>
+#define sll             set<ll,ll>
+#define msll            multiset<ll,ll>
+#define usll             unordered_set<ll>
+#define umsll           unordered_multiset<ll,ll>
+#define mii             map<int,int>
+#define mll_ll           map<long long,long long>
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e9
+#define n_inf	        -1e9
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define rep(x,a,b)	for(int x = a; x < b; x++) 
+#define per(x,a,b)	for(int x = a; x >= b; x--)
+#define yes cout << "YES"<<endl 
+#define no cout << "NO"<<endl
+#define IO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+bool isprime(int x){rep(i,2,sqrt(x)+1){if(x%i==0)return false;}return true;}
+int nsum(int x){return(x*(x+1))/2;}
+int fact(int x){if(x<=1)return 1;else return x*fact(x-1);}
+bool isPalindrome(string s){int n=s.length();rep(i,0,n/2){if(s[i]!=s[n-i-1])return false;}return true;}
+int is_search(int a[],int k,int l,int r){if(l<=r){int mid=(l+r)/2;if(a[mid]==k)return mid;
+        if(a[mid]<k)return is_search(a,k,mid+1,r);else return is_search(a,k,l,mid-1);}else{return -1;}}
+int kidane(int a[],int n){int mx= INT16_MIN;int sum=0;rep(i,0,n){sum+=a[i];mx= max(sum,mx);if(sum<0)
+        {sum=0;}}return mx;}
+
+
+
+
+
+
+void solve()
+{
+    IO
+    w(x)
+    {
+        int n;cin>>n;
+        string s;cin>>s;
+        int a[n];rep(i,0,n)cin>>a[i];
+        int ans=0;
+        bool check=0;
+        rep(i,0,n)
+        {
+            if(s[i]==0)check=1;
+            if(check)
+            {
+                
+            }
+        }
+        cout<<ans<<"\n";
+    }
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
+
+
+/* 
+
+
+//talknew
+
+ */
+
+///////////////////////////////////////////
+//                                       //
+//          CODE BY  @Advik              //
+//          NickName->Fudan              //
+///////////////////////////////////////////
+
+
+
